0b212b6f0e2c474b67b19b0730e1741609f356f9
hcdante <hectorhugoandradelinares@gmail.com>
2023-04-11 12:33:06 -0600

    Se hicieron algunos ejercicios con clases, propiedades, métodos y otros aspectos sobre POO en el archivo main.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,47 @@
-class Person {
-    name="";
-    email="";
-    age=0;
-    resume="";
-
-    printInfo(){
-        console.log(this.name, this.email, this.age, this.resume);
+class Person { /* Las clases inician con mayusculas */
+    /* Propiedaddes */
+    name = "";
+    email = "";
+    age = 0;
+    resumen = "";
+    static total = ""; /* No se puede heredar */
+    constructor(name, email, age, resumen) { /* Método especial */
+        this.name = name; /* el que no tiene this es del constructor */
+        this.email = email.toLowerCase();
+        this.age = (age < 18) ? 18 : age; /* Si la edad es menor de 18, dejarle 18 */
+        this.resumen = resumen;
+        Person.total++; /* Aumenta conforme haya más personas en la clase */
+    }//constructor person
+    /* Es un método de una clase */
+    prinInfo() {
+        console.log(this.name, this.email, this.age, this.resumen);
     }
+}
 
-}//class Person
+class Employee extends Person { /* Una nueva clase que extiende la funcionalidad de la clase Person */
+    department = "";
+    salary = 0.0;
+    constructor(name, email, age, resumen, department, salary) {
+        super(name, email, age, resumen); /* Reutiliza el constructor de la clase superior */
+        this.department = department;
+        this.salary = salary;
+    }//constructor
+    calculateSalary() { /* Método para calcular Salarios mensuales */
+        return ((this.salary * 30) * 1.16).toFixed(2);
+    }//calculateSalary
 
-let john = new Person();
-john.name="John Doe";
-john.age =40;
-john.email="jdoe@gmail.com";
+    prinInfo() {
+        super.prinInfo(); /* Llama al método de la clase superior */
+        console.log(this.department, this.salary, this.calculateSalary());
+    }//printInfo
+}//class Employee
 
-john.printInfo();
+/* Declarar un objeto en base a una clase */
+let Maritere = new Employee("Maritere", "maritere@email.com", 21, "Java FullStack Developer", "IT", 1255.20); /* Se usa el constructor para asiganre parametros */
+let Dora = new Employee("Dora García", "dora.garcía@gmail.com", 16, "FullStack Develop", "Developmet", 1115.25);
+let Vale = new Employee("Valeria", "valeria.arquieta@gmail.com", 16, "FullStack Develop", "Developmet", 1205.25);
+Maritere.prinInfo();
+Dora.prinInfo();
+Vale.printInfo();
+
+Person.prinTotal();
